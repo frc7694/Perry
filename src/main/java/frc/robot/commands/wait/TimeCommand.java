c249407550d8270edf73178742135a75c4b898f6
frc7694 <frc7694@fake.is-a.dev>
2024-03-20 14:58:05 -0400

    robot

---
+++ src/main/java/frc/robot/commands/wait/TimeCommand.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.wait;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class TimeCommand extends CommandBase {
+
+    long start;
+    long length;
+
+    public TimeCommand(long length) {
+        super();
+        this.length = length;
+    }
+
+    @Override
+    public void initialize() {
+        this.start = System.currentTimeMillis();
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return System.currentTimeMillis() - start > length;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return true;
+    }
+}
